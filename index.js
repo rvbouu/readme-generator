ed7154128bd6bca12aceaddf88c20552fedf8163
Vanessa Bou <rvbouu@gmail.com>
2024-04-21 13:07:56 -0500

    commented js files

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+// declaring dependencies and variables
 const { makeLog } = require('./lib/log');
 const { readMeQuestions } = require('./lib/questions');
 
+// function to initialize program
 function init() {
+  // calls readMeQuestions function to prompt the user for input then passes the values to makeLog function
+  // makeLog function saves generated README file to lib folder
   readMeQuestions().then((content) => makeLog('./lib/exampleREADME.md', content));
 }
+
+// calls init() function to run the program
 init();
